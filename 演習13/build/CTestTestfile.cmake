35f1a4bdbb1f22f8d4ab4d31943bf57121fd2cd7
DESKTOP-I8QUV7E\UCRE <sakanishi@ace-software.com>
2026-03-05 15:56:34 +0900

    演習

--- 演習13/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/UCRE/Documents/gtest
-# Build directory: /mnt/c/Users/UCRE/Documents/gtest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
