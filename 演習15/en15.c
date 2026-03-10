bc6b30c728b00b9b41b0cea81e8f77fba7cf2165
DESKTOP-I8QUV7E\UCRE <sakanishi@ace-software.com>
2026-03-10 11:05:18 +0900

    演習

--- 演習15/en15.c
+++ 演習15/en15.c
@@ -19,18 +19,24 @@ int main(void)
         {"onizuka",93},
     };
 
-    list dammy[20];
+    /*int max_score = 0;
+    char fastName[10];*/
+    list dammy = table[0];
     int member = sizeof(table) / sizeof(table[0]);
-    int i,j,k,l,max_score;
-    char fastName[10];
+    int i,j,k = 0;
 
     printf("--------------------名前順--------------------\n");
     
-    for(i = 0; i < member -1; i++)
+
+    /*table[i]以降を比較し最も早い名前(a~z順)
+    とtable[i]番目を入れ替える選択ソート*/
+
+    /*for(i = 0; i < member -1; i++)
     {
         //名前順
         strcpy(fastName,table[i].name);
         k = i;
+
         for(j = i + 1; j < member; j++)
         {
             if (strcmp(table[j].name,fastName) < 0)
@@ -40,21 +46,35 @@ int main(void)
             }
         }
         //データ入れ替え
-        dammy[i] = table[i];
+        dammy = table[i];
         table[i] = table[k];
-        table[k] = dammy[i];
+        table[k] = dammy;
+    }*/
+
+    //バブルソート
+    for (i = 0; i < member - 1; i++) 
+    {
+        for (j = 0; j < member - 1 - i; j++) 
+        {
+            if (strcmp(table[j].name, table[j+1].name) > 0) 
+            {
+                // データ入れ替え
+                dammy = table[j];
+                table[j] = table[j+1];
+                table[j+1] = dammy;
+            }
+        }
     }
-    for(int l = 0; l < member ; l++)
+    for(int i = 0; i < member ; i++)
     {
         //出力
-        printf("%s %d\n",table[l].name,table[l].score);
+        printf("%s %d\n",table[i].name,table[i].score);
     }
 
     printf("--------------------成績順--------------------\n");
 
-    for(i = 0; i < member - 1; i++)
+ /*   for(i = 0; i < member - 1; i++)
     {
-        //年齢順
         strcpy(fastName,table[i].name);
         max_score = table[i].score;
         k = i;
@@ -73,17 +93,44 @@ int main(void)
                     k = j;
                 }
             }
-
         }
         // データ入れ替え
-        dammy[i] = table[i];
+        dammy = table[i];
         table[i] = table[k];
-        table[k] = dammy[i]; 
+        table[k] = dammy; 
+    }*/
+
+        //バブルソート
+    for (i = 0; i < member - 1; i++) 
+    {
+        for (j = 0; j < member - 1 - i; j++) 
+        {
+            if (table[j].score > table[j+1].score) 
+            {
+                // データ入れ替え
+                dammy = table[j];
+                table[j] = table[j+1];
+                table[j+1] = dammy;
+            }
+            else if (table[j].score == table[j+1].score)
+            {
+                //scoreが同じなら名前が早い順
+                if (strcmp(table[j].name, table[j+1].name) > 0) 
+                {
+                    dammy = table[j];
+                    table[j] = table[j+1];
+                    table[j+1] = dammy;
+                }
+                
+            }
+            
+            
+        }
     }
-    for(l = 0; l < member ; l++)
+    for(i = 0; i < member ; i++)
     {
         //出力
-        printf("%s %d\n",table[l].name,table[l].score);
+        printf("%s %d\n",table[i].name,table[i].score);
     }
     return 0;
 }
