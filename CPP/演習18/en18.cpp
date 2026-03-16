144fcdd375dc863ec982dddb2a7b6847b5bdc565
DESKTOP-I8QUV7E\UCRE <sakanishi@ace-software.com>
2026-03-16 14:48:59 +0900

    cpp

--- CPP/演習18/en18.cpp
+++ CPP/演習18/en18.cpp
@@ -6,15 +6,15 @@ class Counter
 private:
     int num;
 public:
-    Counter();
+    Counter(int Num);
 public:
     int Getter();
     void Setter(const int value);
 };
 
-Counter::Counter()
+Counter::Counter(int Num): num(Num)
 {
-    num = 0;
+    cout << "コンストラクタが呼び出されました" << endl;
 }
 
 int Counter::Getter()
@@ -29,7 +29,7 @@ void Counter::Setter(const int value)
 
 int main(void)
 {
-    Counter counter[3];     
+    Counter counter[3] = {Counter(10),Counter(20),Counter(30)};     
     for(int i = 0; i < 3; i++)
     {
         cout << counter[i].Getter() <<  endl; 
