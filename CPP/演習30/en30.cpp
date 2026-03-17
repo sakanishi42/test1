5e4eebd0b7f21445abd4250857e23fb96e1ee2d2
DESKTOP-I8QUV7E\UCRE <sakanishi@ace-software.com>
2026-03-17 14:04:25 +0900

    cpp

---
+++ CPP/演習30/en30.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//演習２６と同じソースコード。
+
+class Animal
+{
+private:
+    string name_;
+    int age_;
+public:
+    Animal(string name, int age);
+    void show();
+};
+
+Animal::Animal(string name, int age): name_(name), age_(age){};
+
+void Animal::show()
+{
+    cout << "名前:" << name_ << " |年齢:" << age_ << "歳" << endl;
+}
+
+class Dog: public Animal
+{
+public:
+//演習３０の内容↓
+    Dog(string name, int age) : Animal(name, age) {}
+};
+
+int main(void)
+{
+    Dog dog("太郎",6);
+    dog.show();
+    return 0;
+}
