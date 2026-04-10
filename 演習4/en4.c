54f6d731efcc5d272a368dbcfc57dc264b3996e0
sakanishi42 <sakanishi@ace-software.com>
2026-04-10 13:33:30 +0900

    Update en4.c

--- 演習4/en4.c
+++ 演習4/en4.c
@@ -3,9 +3,9 @@ int main(void)
 {
     double data ,sum = 0;
     int i = 0;
+    printf("入力された数字の平均値を求め、０が入力されると終了します。\n");
     while(1)
     {
-        printf("入力された数字の平均値を求め、０が入力されると終了します。\n");
         printf("数字を入力して下さい。\n");
         scanf("%lf",&data);
         if(data == 0)
